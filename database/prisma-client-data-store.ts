856ac783bf2e25dda2cffb78b71489ce4e36a813
chulakasam <sampathchulaka33872@gmail.com>
2025-02-22 20:55:00 +0530

    added client done

--- database/prisma-client-data-store.ts
+++ database/prisma-client-data-store.ts
@@ -5,8 +5,21 @@ const prisma = new PrismaClient();
 
 export async function AddClient(newClient:Client){
     try{
-
+        const addedClient=await prisma.client.create({
+            data:{
+                id:newClient.id,
+                name:newClient.name,
+                height:newClient.height,
+                weight:newClient.weight,
+                age:newClient.age,
+                dob:newClient.dob,
+                gender:newClient.gender,
+                image:newClient.image
+            }
+        });
+        console.log('client added successfully',addedClient);
+        return addedClient;
     }catch (error){
-
+        console.log('error added client');
     }
 }
--- server.ts
+++ server.ts
@@ -4,6 +4,17 @@ import clientRoutes from "./routes/client-routes";
 const app = express();
 
 app.use(express.json());
+
+app.use('/',(req,res,next)=>{
+     res.header("Access-Control-Allow-Origin", "*");
+     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, content-type');
+
+     next();
+})
+
+
+
 app.use('/client',clientRoutes);
 
 app.listen('3000',()=>{
