691fa438d94c281294823e9512e7b1b19fb03c5f
chulakasam <sampathchulaka33872@gmail.com>
2025-02-27 15:58:19 +0530

    added authentication

---
+++ database/user-prisma-data-store.ts
@@ -0,0 +1,28 @@
+import {PrismaClient, User} from '@prisma/client';
+import bcrypt from "bcrypt";
+
+
+const prisma = new PrismaClient();
+
+export async function createUser(user: { password: string; email: string}) {
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+
+    const addedUser = await prisma.user.create({
+        data: {
+            email : user.email,
+            password : hashedPassword,
+        },
+    });
+    console.log("User created:", addedUser);
+}
+
+export async function verifyUserCredentials(verifyUser: { password: any; email: any }) {
+    const user : User | null = await prisma.user.findUnique({
+        where: { email: verifyUser.email },
+    });
+    if (!user) {
+        return false;
+    }
+
+    return await bcrypt.compare(verifyUser.password, user.password);
+}
--- server.ts
+++ server.ts
@@ -1,23 +1,40 @@
 import express from 'express';
 import clientRoutes from "./routes/client-routes";
 import workoutRoutes from "./routes/workout-routes";
+import authRoutes, {authenticateToken} from "./routes/authRoutes";
+import dotenv from "dotenv";
+import cors from 'cors';
+
+
 
 const app = express();
 
 app.use(express.json());
+dotenv.config();
 
-app.use('/',(req,res,next)=>{
-     res.header("Access-Control-Allow-Origin", "*");
-     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, content-type');
-
-     next();
-})
-
+app.use(cors({
+    origin: 'http://localhost:8081',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 
+// app.use('/',(req,res,next)=>{
+//      res.header("Access-Control-Allow-Origin", "*");
+//      res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+//      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, content-type');
+//
+//      next();
+// })
+
+
+app.use('/auth',authRoutes);
 app.use('/client',clientRoutes);
 app.use('/workout',workoutRoutes);
 app.listen('3000',()=>{
     console.log('server running in port 3000');
 });
+
+console.log("Loaded SECRET_KEY:", process.env.SECRET_KEY);
+app.use(authenticateToken);
